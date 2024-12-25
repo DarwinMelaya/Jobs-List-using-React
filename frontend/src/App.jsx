924acd2999b2f13687060c77c8ace9873ae9ad7a
DarwinMelaya <116789003+DarwinMelaya@users.noreply.github.com>
2024-12-25 10:48:12 +0800

    Home Page done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Hero from "./components/Hero";
 import HomeCards from "./components/HomeCards";
 import JobListings from "./components/JobListings";
 import ViewAllJobs from "./components/ViewAllJobs";
+
 const App = () => {
   return (
     <>
